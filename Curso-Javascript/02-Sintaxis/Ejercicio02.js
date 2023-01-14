88c6cc0c006fa0c28ca2f12b2d35b82715ff1ae8
Kiara Mimbela Vasquez <115656708+KiaraMimbela@users.noreply.github.com>
2023-01-14 18:30:58 -0300

    Ejercicio 2

---
+++ Curso-Javascript/02-Sintaxis/Ejercicio02.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Kiara",
+    21,
+    true,
+    new Date(2002, 0, 8),
+    {
+        titulo: "Cariño, cuánto te odio",
+        autor: "Sally Thorne",
+        fecha: new Date(2016, 7, 9),
+        url: "https://www.buscalibre.cl/libro-carino-cuanto-te-odio-sally-thorne-espasa/9788467050455/p/48513480"
+    }
+];
