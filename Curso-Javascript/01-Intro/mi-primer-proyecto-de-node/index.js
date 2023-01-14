d8b5d70984604a239042f908a0b9f0df23c61b8c
Kiara Mimbela Vasquez <115656708+KiaraMimbela@users.noreply.github.com>
2023-01-14 16:00:22 -0300

    01-Intro JS

---
+++ Curso-Javascript/01-Intro/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,9 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*Este es la parte intermedia de nuestro proyecto
+En esta parte realizamos los chequeos necesarios
+Por último vamos a la última línea de saludo*/
+
+// Este va ser el mensaje que se envia para despedir
+console.log("Adiós");
