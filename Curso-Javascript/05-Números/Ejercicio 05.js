7ca22ec58c56e2c0107483d857ca4f13f43b8a7c
KiaraMimbela <kiaramimbelavasquez@gmail.com>
2023-01-17 21:18:33 -0300

    Ejercicio 05

---
+++ Curso-Javascript/05-Números/Ejercicio 05.js
@@ -0,0 +1,10 @@
+let cm_altura = 155;
+let m_altura = 1.55;
+let peso = 56.8;
+
+// altura redondeada hacia arriba
+let altura = Math.ceil(m_altura);
+// peso redondeado hacia abajo
+let peso2 = Math.floor(peso);
+
+let iguales = Number.MAX_VALUE + 1 === Number.MIN_VALUE;
