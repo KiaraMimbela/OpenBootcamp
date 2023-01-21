78c67045320addc506f691ff07032691706cb8be
KiaraMimbela <kiaramimbelavasquez@gmail.com>
2023-01-20 22:07:41 -0300

    Ejercicio Conjuntos

---
+++ Curso-Javascript/06-Sets_Objetos/Ejercicios/Conjuntos.js
@@ -0,0 +1,8 @@
+// set con los nombres de mi familia
+const familia = new Set(["Laura", "Victor", "Christian", "Kiara", "Matthew", "Naomi", "Juan"]);
+
+// agregando mi nombre (duplicado)
+familia.add("Naomi");
+
+familia.add("Javascrip");
+console.log(familia);
