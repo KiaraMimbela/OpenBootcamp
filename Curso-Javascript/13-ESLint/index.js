d4e04ff47304d8cdce2fe91fa733bfd95fe2a484
KiaraMimbela <kiaramimbelavasquez@gmail.com>
2023-01-27 17:48:31 -0300

    Uso de ESLint

---
+++ Curso-Javascript/13-ESLint/index.js
@@ -0,0 +1,23 @@
+const persona = "Naomi"
+
+// deshabilitar las reglas de eslint
+/* eslint-disable*/
+const persona2 = 'Gonzalo';
+/* eslint-enable */
+
+// deshabilitar las reglas de eslint en una sola linea
+const str = 'Esto es un string'; // eslint-disable-line
+
+// deshabilitar las reglas de eslint en la siguiente linea
+/* eslint-disable-next-line*/
+const str2 = 'Otro string'
+
+const hola = "holas"
+
+console.log("hola")
+
+const persona3 = "Marian"
+
+const nombre3 = "Julian"
+
+console.log(nombre3)
